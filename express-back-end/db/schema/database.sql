177891e489b12f53bcb6b144e0f7a5e89ee83163
djepi29 <j3anpi3rr3g3org3s@email.com>
2023-08-02 22:56:15 -0400

    update tutors table in database with avg_rating avg_class_prices number_of_booked_students_per_tutor

--- express-back-end/db/schema/database.sql
+++ express-back-end/db/schema/database.sql
@@ -5,6 +5,7 @@ DROP TABLE IF EXISTS tutors CASCADE;
 DROP TABLE IF EXISTS classes CASCADE;
 DROP TABLE IF EXISTS bookings CASCADE;
 DROP TABLE IF EXISTS messages CASCADE;
+DROP TABLE IF EXISTS reviews CASCADE;
 
 -- CREATE DATABASE learningstation;
 
@@ -28,7 +29,10 @@ CREATE TABLE tutors (
   email VARCHAR(255) UNIQUE NOT NULL,
   location VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
-  image TEXT
+  image TEXT,
+  avg_rating NUMERIC DEFAULT 0,
+  avg_class_prices MONEY DEFAULT 0,
+  num_students_booked INTEGER DEFAULT 0
 );
 
 -- Create the "classes" table
@@ -57,5 +61,119 @@ CREATE TABLE messages (
   sent_at TIMESTAMP DEFAULT NOW(),
   payment_confirmed BOOLEAN DEFAULT FALSE
 );
+-- the reviews table
+CREATE TABLE reviews (
+  review_id SERIAL PRIMARY KEY,
+  student_id INT REFERENCES students(student_id) ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED,
+  tutor_id INT REFERENCES tutors(tutor_id) ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED,
+  rating INT NOT NULL,
+  comment TEXT,
+  review_date TIMESTAMP DEFAULT NOW() NOT NULL
+);
 
 
+
+------------------- TRIGGER FUNCTION FOR AVG_RATING ------------------------
+
+
+CREATE OR REPLACE FUNCTION update_avg_rating()
+RETURNS TRIGGER AS
+$$
+BEGIN
+  IF TG_OP = 'INSERT' THEN
+    UPDATE tutors
+    SET avg_rating = (
+      SELECT COALESCE(AVG(rating), 0)
+      FROM reviews
+      WHERE tutor_id = NEW.tutor_id
+    )
+    WHERE tutor_id = NEW.tutor_id;
+  ELSIF TG_OP = 'UPDATE' THEN
+    UPDATE tutors
+    SET avg_rating = (
+      SELECT COALESCE(AVG(rating), 0)
+      FROM reviews
+      WHERE tutor_id = NEW.tutor_id
+    )
+    WHERE tutor_id = NEW.tutor_id;
+  END IF;
+  RETURN NEW;
+END;
+$$
+LANGUAGE plpgsql;
+---- TRIGGER
+CREATE TRIGGER update_avg_rating_trigger
+AFTER INSERT OR UPDATE ON reviews
+FOR EACH ROW
+EXECUTE FUNCTION update_avg_rating();
+
+
+----------------- TRIGGER FUNCTION FOR CLASS PRICES ---------------
+
+-- Create a new function to update avg_class_prices
+CREATE OR REPLACE FUNCTION update_avg_class_prices()
+RETURNS TRIGGER AS
+$$
+BEGIN
+  UPDATE tutors
+  SET avg_class_prices = COALESCE((
+      SELECT AVG(class_price::numeric)
+      FROM classes
+      WHERE classes.tutor_id = NEW.tutor_id
+  ), 0)
+  WHERE tutors.tutor_id = NEW.tutor_id;
+
+  RETURN NEW;
+END;
+$$
+LANGUAGE plpgsql;
+
+-- Create a trigger to update avg_class_prices
+
+CREATE TRIGGER update_avg_class_prices_trigger
+AFTER INSERT OR UPDATE ON classes
+FOR EACH ROW
+EXECUTE FUNCTION update_avg_class_prices();
+
+------------- TRIGGER FUNCTION FOR TOTAL NUMBER OFF BOOKED STUDENTS -------
+
+-- Create the trigger function
+CREATE OR REPLACE FUNCTION update_num_students_booked()
+RETURNS TRIGGER AS
+$$
+BEGIN
+  UPDATE tutors
+  SET num_students_booked = COALESCE((
+    SELECT COUNT(DISTINCT bookings.student_id)
+    FROM bookings
+    INNER JOIN classes ON bookings.class_id = classes.class_id
+    WHERE classes.tutor_id = tutors.tutor_id
+  ), 0);
+
+  RETURN NEW;
+END;
+$$
+LANGUAGE plpgsql;
+
+---
+DROP FUNCTION IF EXISTS update_num_students_booked();
+
+-- Create a trigger to update num_students_booked
+CREATE TRIGGER update_num_students_booked_trigger
+AFTER INSERT OR UPDATE ON bookings
+FOR EACH STATEMENT
+EXECUTE FUNCTION update_num_students_booked();
+----
+DROP TRIGGER IF EXISTS update_num_students_booked_trigger ON bookings;
+
+--------------------------------------------------------------------------------
+
+-- CHECKING TRIGGER 
+ SELECT tgname AS trigger_name
+FROM pg_trigger
+WHERE tgname = 'update_num_students_booked_trigger';
+
+-- CHECKING TRIG FUNCTION
+SELECT proname AS function_name
+FROM pg_proc
+WHERE proname = 'function_name_here';
