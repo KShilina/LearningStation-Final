b50345abf2bab4ee5c1c64091deb9e49d581ae9f
Ekaterina <yatakshilina@gmail.com>
2023-08-07 17:48:40 -0400

    add stripe

---
+++ react-front-end/src/components/Profile.js
@@ -0,0 +1,41 @@
+import React, {useState} from "react";
+
+import StudentRegister from "./StudentRegister";
+import TutorRegister from "./TutorRegister";
+import { useAuth0 } from "@auth0/auth0-react";
+
+const Profile = (props) =>{
+  const [userType, setUserType] = useState(null);
+  const {user} = useAuth0()
+
+  const handleSelectChange = (event) => {
+    setUserType(event.target.value);
+  };
+
+  if (userType === "tutor"){
+    return <TutorRegister />
+  }
+  if (userType === "student"){
+    return <StudentRegister />
+  }
+// if (userType === null){
+  return (<div className="signup-dropdown">
+  <select
+    id="signup-dropdown"
+    onChange={handleSelectChange}
+    value={userType}
+  >
+    <option value="">Choose from</option>
+    <option value="student">Become a Student</option>
+    <option value="tutor">Become a Tutor</option>
+    
+  </select>
+  </div>  )
+// }
+ 
+}
+
+
+  
+
+export default Profile
