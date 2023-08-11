496a96d54a64479928e0c1d5f3506f52cb4a81dc
Ekaterina <yatakshilina@gmail.com>
2023-08-11 12:16:30 -0400

    add styles to StudentBookings page

--- react-front-end/src/components/StudentBookings.js
+++ react-front-end/src/components/StudentBookings.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Footer from "./Footer";
 import "./StudentBookings.scss";
+import { Link } from "react-router-dom";
 
 const StudentBookings = () => {
   const [bookings, setBookings] = useState([]);
@@ -12,8 +13,10 @@ const StudentBookings = () => {
     // Function to fetch bookings related to the logged-in student
     const fetchStudentBookings = async () => {
       try {
-        const studentId = sessionStorage.getItem('student_id');
-        const response = await axios.get(`/api/bookings/students/${studentId}/bookings`);
+        const studentId = sessionStorage.getItem("student_id");
+        const response = await axios.get(
+          `/api/bookings/students/${studentId}/bookings`
+        );
         setBookings(response.data);
       } catch (error) {
         console.error("Error fetching bookings:", error);
@@ -26,33 +29,61 @@ const StudentBookings = () => {
   return (
     <section>
       <div class="header-text">
-          <img
-            class="header-image-std-bookings"
-            src={
-              process.env.PUBLIC_URL +
-              "/images/thought-catalog-505eectW54k-unsplash.jpg"
-            }
-            alt="headerImg"
-          />
+        <img
+          class="header-image-std-bookings"
+          src={
+            process.env.PUBLIC_URL +
+            "/images/thought-catalog-505eectW54k-unsplash.jpg"
+          }
+          alt="headerImg"
+        />
 
-          <h1>My Bookings</h1>
-        </div>
-      
+        <h1>My Bookings</h1>
+      </div>
+
       {bookings.length === 0 ? (
         <p>No bookings</p>
       ) : (
-        <ul>
-          {bookings.map((booking) => (
-            <li key={booking.booking_id}>
-              <p>Booking ID: {booking.booking_id}</p>
-              <p>Class ID: {booking.class_id}</p>
-              <p>Booking Date: {booking.booking_date}</p>
-              <p>Payment Confirmed: {booking.payment_confirmed}</p>
-              <hr />
-            </li>
-          ))}
-        </ul>
+        <div className="booking-table">
+          <h1>Booking Details</h1>
+          <table>
+            <thead>
+              <tr>
+                <th>Booking ID</th>
+                <th>Class ID</th>
+                <th>Booking Date</th>
+                <th>Payment Confirmed</th>
+              </tr>
+            </thead>
+            <tbody>
+              {bookings.map((booking) => (
+                <tr key={booking.booking_id}>
+                  <td>{booking.booking_id}</td>
+                  <td>{booking.class_id}</td>
+                  <td>{booking.booking_date}</td>
+                  <td>{booking.payment_confirmed ? "❌" : "✅"}</td>{" "}
+                  {/* Conditional rendering */}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       )}
+      
+      <nav>
+        <ul className="student-nav-links">
+          {/* Use Link instead of a */}
+          <li>
+            <Link to="/StudentMessages">Messages</Link>
+          </li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/logout">Log out</Link>
+          </li>
+        </ul>
+      </nav>
       <Footer />
     </section>
   );
