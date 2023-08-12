b479c4645a7b2bb863e5f30d8e6d93b6fc0feb2c
Daniel Soares <danielsoares01@outlook.com>
2023-08-11 20:41:44 -0700

    cahges to tutor cards and home styling

--- react-front-end/src/components/TutorCard.js
+++ react-front-end/src/components/TutorCard.js
@@ -8,15 +8,16 @@ const TutorCard = ({ tutor, classes }) => {
     <Link to={`/tutors/${tutor.tutor_id}`} className="tutor-card">
       <div className="tutor-info">
         <h3>{tutor.name}</h3>
-        <p>Name: {tutor.first_name} {tutor.last_name}</p>
+        <p class="tutor-card-name" >{tutor.first_name} {tutor.last_name}</p>
         <p>Expertise: {tutor.expertise}</p>
-        <p>About_Me: {tutor.quick_bio}</p>
-        <p>location: {tutor.location}</p>
+        <p>About Me: {tutor.quick_bio}</p>
+        <p>Location: {tutor.location}</p>
         {/* Add more tutor information here as needed */}
       </div>
       <img className="tutor-image" src={tutor.image} alt={`${tutor.name}'s pic`} />
     </Link>
   );
 };
+
 
 export default TutorCard;
