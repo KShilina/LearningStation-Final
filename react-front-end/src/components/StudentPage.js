20e2a4b707c9147ac7fccb1120899c398a77baa2
Ekaterina <yatakshilina@gmail.com>
2023-08-14 21:32:27 -0400

    add style to navbar

--- react-front-end/src/components/StudentPage.js
+++ react-front-end/src/components/StudentPage.js
@@ -3,22 +3,20 @@ import "./StudentPage.scss";
 import { Link } from "react-router-dom"; // Import the Link component
 import { useAuth0 } from "@auth0/auth0-react";
 import Footer from "./Footer";
+import Navbar from "./Navbar";
 
 const StudentPage = () => {
+  const { user, isAuthenticated, isLoading } = useAuth0();
+  console.log(user, "user");
 
-  const {user, isAuthenticated, isLoading} = useAuth0()
-  console.log(user, "user")
+  useEffect(() => {}, [isAuthenticated]);
 
-  useEffect(() => {
-    
-  
-    
-  }, [isAuthenticated])
-  
   return (
-    isAuthenticated && !isLoading && (
-    <div>
-      <div className="header-text">
+    isAuthenticated &&
+    !isLoading && (
+      <div>
+        <Navbar />
+        <div className="header-text">
           <img
             className="header-image-std"
             src={
@@ -30,64 +28,22 @@ const StudentPage = () => {
 
           <h1>Welcome to Student Page</h1>
         </div>
-      
-      <nav>
-        <ul className="student-nav-links">
-          {/* Use Link instead of a */}
-          <li>
-            <Link to="/StudentMessages">Messages</Link>
-          </li>
-          <li>
-            <Link to="/StudentBookings">Bookings</Link>
-          </li>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/logout">Log out</Link>
-          </li>
-        </ul>
-      </nav>
-      <Footer />
-    </div>
-  ))
+
+        <nav>
+          <ul className="student-nav-links">
+            {/* Use Link instead of a */}
+            <li>
+              <Link to="/StudentMessages">Messages</Link>
+            </li>
+            <li>
+              <Link to="/StudentBookings">Bookings</Link>
+            </li>
+          </ul>
+        </nav>
+        <Footer />
+      </div>
+    )
+  );
 };
 
 export default StudentPage;
-
-// //KATYA code snippet
-
-// import React from "react";
-// import "./StudentPage.scss";
-// import { Link } from "react-router-dom"; // Import the Link component
-
-// const StudentPage = () => {
-//   return (
-//     <div>
-//       <h1 className="header">Welcome</h1>
-//       <nav>
-//         <ul className="nav-links">
-//           {/* Use Link instead of a */}
-//           <li>
-//             <Link to="/StudentMessages">Messages</Link>
-//           </li>
-//           <li>
-//             <Link to="/StudentBookings">Bookings</Link>
-//           </li>
-//           <li>
-//             <Link to="/">Home</Link>
-//           </li>
-//           <li>
-//             <Link to="/logout">Log out</Link>
-//           </li>
-//           <li>
-//             <Link to="/profile">My Profile</Link>
-//           </li>
-//         </ul>
-//       </nav>
-//     </div>
-//   );
-// };
-
-// export default StudentPage;
-
