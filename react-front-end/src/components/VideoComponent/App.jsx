f72cf2cdc6b8a6541ef1f2802e440cec39aa1468
Daniel Soares <danielsoares01@outlook.com>
2023-08-16 20:51:49 -0700

    style to the video chat page

--- react-front-end/src/components/VideoComponent/App.jsx
+++ react-front-end/src/components/VideoComponent/App.jsx
@@ -37,20 +37,36 @@ import VideoPlayer from './VideoPlayer';
 import Sidebar from './Sidebar';
 import Notifications from './Notifications';
 import './App.scss'; // Import the SCSS file
+import Navbar from '../Navbar';
+import Footer from '../Footer';
 
 const theme = createTheme();
 
 const Component = () => {
   return (
-    <div className="appContainer">
-      <AppBar className="appBar" position="static" color="inherit">
-        <Typography variant="h2" align="center">Video Chat</Typography>
-      </AppBar>
-      <VideoPlayer />
-      <Sidebar>
-        <Notifications />
-      </Sidebar>
-    </div>
+    <>
+      <Navbar />
+
+      <div className="appContainer">
+      
+        <AppBar className="appBar" position="static" color="inherit">
+          <Typography variant="h2" align="center">Video Chat</Typography>
+        </AppBar>
+        <VideoPlayer />
+        <Sidebar>
+          <Notifications />
+        </Sidebar>
+      </div>
+
+      <footer className="video-footer">
+      <div className="footer-content">
+        123 Demo Street, Toronto ON   | ☎  +1 (647) 000-0000   |   email@learningStation.ca
+        <br/>
+        © {new Date().getFullYear()} Learning Station. All rights reserved.
+      </div>
+    </footer>
+
+    </>
   );
 };
 
